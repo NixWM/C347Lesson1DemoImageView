be219a572034f68d0da5e22498373eac034662c2
Nix WM <16046530@myrp.edu.sg>
2021-04-20 11:02:32 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/ro/c346/id16046530/demoimageview/ListView.java
@@ -0,0 +1,14 @@
+package sg.edu.ro.c346.id16046530.demoimageview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+}
